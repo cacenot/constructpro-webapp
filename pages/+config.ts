91c0ee5d7060dc5f3cc5e670eb68aa71fbdd8ef8
Fernando <cacenot@gmail.com>
2026-04-08 10:08:37 -0300

    fix: habilitar prerender parcial do Vike e apontar Firebase Hosting para dist/client

--- pages/+config.ts
+++ pages/+config.ts
@@ -4,4 +4,5 @@ import vikeReact from 'vike-react/config'
 export default {
   extends: vikeReact,
   ssr: false, // SPA mode - disable server-side rendering
+  prerender: { partial: true }, // Generate static HTML for Firebase Hosting (skip dynamic routes)
 } satisfies Config
